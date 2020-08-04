bcb1ad99a4c6cd11f1e5acaa1c2e6370c3b00c8c
Edward Ceballos Agudelo <edwardceballosagudelo@gmail.com>
2020-08-03 23:07:31 -0500

    creacion de vista de posts

---
+++ instagram/urls.py
@@ -0,0 +1,8 @@
+"""instagram URLs module."""
+from django.urls import path
+
+from posts import views as posts_views
+
+urlpatterns = [
+    path('posts/', posts_views.list_posts),
+]
---
+++ posts/apps.py
@@ -0,0 +1,8 @@
+""" Post applications module."""
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    """Post applications settings."""
+    name = 'posts'
+    varbose_name = 'Posts'
